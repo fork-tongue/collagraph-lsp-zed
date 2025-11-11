714e39a81b12e5303cdbddfe98abbc311962de2c
Berend Klein Haneveld <berendkleinhaneveld@gmail.com>
2025-11-11 22:09:16 +0100

    Improve indent by copying from vue plugin

--- languages/collagraph/indents.scm
+++ languages/collagraph/indents.scm
@@ -1,18 +1,4 @@
 ; Indentation rules for Collagraph templates
-
-; Indent element contents
 (element
   (start_tag) @start
-  (end_tag)? @end) @indent
-
-(element
-  (start_tag) @start
-  (erroneous_end_tag)? @end) @indent
-
-; Indent multi-line start tags
-(start_tag
-  "<" @start
-  ">" @end) @indent
-
-; Self-closing tags don't affect indentation
-(self_closing_tag) @_ignore
+  [(end_tag) (erroneous_end_tag)]? @end) @indent
